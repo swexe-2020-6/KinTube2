72b9377104da768e3873025223616493dc7e0c54
Ryukei Hayashida <1811140071m@ed.fuk.kindai.ac.jp>
2020-11-25 06:48:48 +0000

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
     resources :images
     root 'images#index'
     get 'get_image/:id', to: 'images#get_image'
+    get 'top/main'
+    post 'top/login'
+    root 'top#main'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
