b615ecf4c23426dbc77df9e90e584809d5919e69
EC2 Default User <ec2-user@ip-172-31-61-171.ec2.internal>
2020-11-25 05:45:15 +0000

    画像アップロード

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,42 @@
 class ImagesController < ApplicationController
+    def index
+        @images = Image.all
+    end
+    
+    def new
+        @image = Image.new
+    end
+    
+    def create
+        title = params[:image][:title]
+        file = params[:image][:file].read
+        image = Image.new(title: title, file: file)
+        image.save
+        redirect_to '/'
+    end
+    
+    def destroy
+        image = Image.find(params[:id])
+        image.destroy
+        redirect_to '/'
+    end
+    
+    def edit
+        @image = Image.find(params[:id])
+    end
+    
+    def update
+        image = Image.find(params[:id])
+        file = params[:image][:file].read
+        title = params[:image][:title]
+        image.update(title: title, file: file)
+        redirect_to '/'
+    end
+    
+    def get_image
+        image = Image.find(params[:id])
+        send_data image.file, disposition: :inline, type: 'image/png'
+    end
+    
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     resources :images
     root 'images#index'
+    get 'get_image/:id', to: 'images#get_image'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
